5fa62c9e68a784dc9be7a14c46ff2b26138f078f
Daianne <daianasenese@gmail.com>
2023-01-25 19:00:05 +0100

    prueba

---
+++ Front/ProyectosPython/proyecto1.py
@@ -0,0 +1,21 @@
+# Proyecto 1: Historias locas
+
+tiempo = input ("Tiempo: ")
+tiempo2 = input ("Tiempo: ")
+tiempo3 = input ("Tiempo: ")
+sustantivo = input ("Sustantivo: ")
+sustantivo2 = input ("Sustantivo: ")
+sustantivo3 = input ("Sustantivo: ")
+sustantivo4 = input ("Sustantivo: ")
+adjetivo = input ("Adjetivo: ")
+adjetivo2 = input ("Adjetivo: ")
+emocion = input ("Emoción: ")
+emocion2 = input ("Emoción: ")
+emocion3 = input ("Emoción: ")
+verbo = input ("Verbo: ")
+verbo2 = input ("Verbo: ")
+verbo3 = input ("Verbo: ")
+
+historia = f"Me siento feliz con mi {tiempo}, he pensado mucho en este momento, he masticado {sustantivo}, he captado distintas experiencias y sacado conclusiones sobre lo intangible. Hoy miro hacia {tiempo2} y entiendo toda la {sustantivo2}, el {sustantivo3}, la {sustantivo4} y ansiedad que tuve que afrontar para llegar adonde estoy, para entender los porqués, para asimilar mejor, para no enojarme ni frustrarme con los contratiepos porque {tiempo3} comprendo que cuando tenga que ser, será sin más. Y si bien me estoy {verbo} y {verbo2} mi camino, debo relajarme con las cosas que se escapan de mis manos, no hacerme cargo de lo que no es mi responsabilidad para poder fluir como el agua. Intento y trabajo todos los días por eliminar los rencores y malos pensamientos o sensaciones negativas ya que no me construyen o sólo originan un malestar y un prejuicio y gracias a todo esto reconfirmo mi poder y determinio. Me siento {emocion}, {emocion2} y {emocion3}, una eterna aprendíz agradecida a la vida por enseñarme tanto y me auguro todo lo {adjetivo}, lo {adjetivo2} y las adversidades necesarias para poder seguir {verbo3}."
+
+print (historia)
